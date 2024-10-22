4d4a6eb9e6b8f07b44f1c065e870f3525ba03928
nishi10000 <sanndonomesiyorisanndogasuki@yahoo.co.jp>
2024-10-22 20:35:56 +0900

    voice_commend.pyテスト用で作成

--- src/voice_commands.py
+++ src/voice_commands.py
@@ -1,1 +1,14 @@
 # voice_commands.py: 音声認識を通じてコマンドを受け取り、操作を行うモジュール
+
+import speech_recognition as sr
+
+listener = sr.Recognizer()
+
+try:
+    with sr.Microphone() as source:
+        print("Listening...")
+        voice = listener.listen(source)
+        voice_text = listener.recognize_google(voice, language="ja-JP")
+        print(voice_text)
+except:
+    print('Sorry, I could not listen')
